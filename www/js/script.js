487d2325f183db1d2878cdeb52086b9aba6f8959
awkwardhandshake <tore@awkwardhandshake.se>
2014-01-19 21:55:23 +0100

    bara ett request till iframe

--- www/js/script.js
+++ www/js/script.js
@@ -44,8 +44,8 @@ bytiframe()
 
 function bytiframe(){
 	  var hej1 = document.getElementById('hej1').src=localStorage.urlen;
-function tvattstuga(){
-	var hej1 = document.getElementById('hej1').src="http://www.sgsstudentbostader.se/ext_gw.aspx?module=wwwash&lang=se#lblPanelName";
+//function tvattstuga(){
+//	var hej1 = document.getElementById('hej1').src="http://www.sgsstudentbostader.se/ext_gw.aspx?module=wwwash&lang=se#lblPanelName";
 	
 	
 }	
