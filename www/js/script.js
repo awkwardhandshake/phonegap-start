5d040b2c0b7e9a557f580224f713fbb2cf2c7f4f
awkwardhandshake <tore@awkwardhandshake.se>
2014-01-20 11:00:06 +0100

    delay = 3000

--- www/js/script.js
+++ www/js/script.js
@@ -36,10 +36,12 @@ bytiframe();
 }
 
 function bytiframe(){
-	  var hej1 = document.getElementById('hej1').src=localStorage.urlen;
-tvattstuga();
-}
-function tvattstuga(){
+	  var hej1 = document.getElementById('hej1').src=localStorage.urlen
+//tvattstuga()
+
+
+setTimeout((function tvattstuga(){
 var hej1 = document.getElementById('hej1').src="http://www.sgsstudentbostader.se/ext_gw.aspx?module=wwwash&lang=se#lblPanelName";
 		
-}	
+}),3000)
+}
