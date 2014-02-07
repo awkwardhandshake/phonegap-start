2b4ec4544200a4940808f38a8b62a76983758346
awkwardhandshake <tore@awkwardhandshake.se>
2014-02-07 21:14:59 +0100

    fix 2

--- www/js/historik.js
+++ www/js/historik.js
@@ -32,15 +32,15 @@ console.log("Längden på array EFTER " + kommaihag.length);
 	var navigeringkul = navigering.setAttribute('onmousedown', kommaihag[(kommaihag.length - 1)]);
 	navigering.setAttribute("class", "Nu har if-satsen ändrat");
 	var backbutton = document.getElementById('backscript');
-	backbutton.innerHTML = 
+	backbutton.innerHTML =
 	'function onLoad() {'+
 'console.log("Går in i onLoad");'+
-        'document.addEventListener("deviceready", onDeviceReady, false);'+
-    '}'+
+'document.addEventListener("deviceready", onDeviceReady, false);'+
+        '}'+
     'function onDeviceReady() {'+
-'console.log("Går in i DeviceReady");'+    	
-        'document.addEventListener("backbutton", function() {'+kommaihag[kommaihag.length-1]+'};, false);'+
-    '}'
+    'console.log("Går in i DeviceReady");'+
+'document.addEventListener("backbutton", function() {'+kommaihag[kommaihag.length-1]+'};, false);'+
+'}';
 	sesam = 1;
 	console.log(sesam);
 	position = 0;
@@ -68,15 +68,15 @@ console.log(kommaihag);
 //back.setAttribute("Kul", "Detta");
 //document.addEventListener("backbutton", kommaihag[kommaihag.length-2], false);
 	var backbuttonelse = document.getElementById('backscript');
-	backbuttonelse.innerHTML = 
-		'function onLoad() {'+
+	backbuttonelse.innerHTML =
+	'function onLoad() {'+
 'console.log("Går in i onLoad");'+
-        'document.addEventListener("deviceready", onDeviceReady, false);'+
-    '}'+
-    'function onDeviceReady() {'+
-'console.log("Går in i DeviceReady");'+    	
-        'document.addEventListener("backbutton", function() {'+kommaihag[kommaihag.length-2]+'};, false);'+
-    '}'
+'document.addEventListener("deviceready", onDeviceReady, false);'+
+'}'+
+'function onDeviceReady() {'+
+'console.log("Går in i DeviceReady");'+
+'document.addEventListener("backbutton", function() {'+kommaihag[kommaihag.length-2]+'};, false);'+
+'}';
 	var navigering = document.getElementById('navigering')
 	var navigeringkul = navigering.setAttribute('onmousedown', kommaihag[(kommaihag.length-2)]);
 	navigering.setAttribute("class", "null")
